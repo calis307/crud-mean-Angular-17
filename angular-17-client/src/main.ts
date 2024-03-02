66a7b729922a08a382055be84b7ebce14918e2a9
calis307 <camilotero89@gmail.com>
2024-03-02 08:20:42 +0100

    Primer commit, actualizado a Angular 17 con nueva sintaxis ,standalone y app.routes.ts

---
+++ angular-17-client/src/main.ts
@@ -0,0 +1,21 @@
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { routes } from './app/app.routes'
+import { provideRouter } from '@angular/router';
+
+
+bootstrapApplication(AppComponent, {
+    providers: [
+       
+        importProvidersFrom(BrowserModule, FormsModule),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(routes)
+        
+
+    ]
+})
+  .catch(err => console.error(err));
